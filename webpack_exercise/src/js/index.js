19018d2f61814b302c77b939cdaa174269618480
Mengti Sun <mengtisun@gmail.com>
2019-12-24 13:39:42 +1100

    rename folder

---
+++ webpack_exercise/src/js/index.js
@@ -0,0 +1,18 @@
+import '../less/1.less';
+
+// import '../css/main.css';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import $ from 'jquery';
+
+// $(function() {
+//   $('body').css('background', '#CCC');
+
+//   console.log(process.env.NODE_ENV)
+// })
+
+let a = 12;
+let b = 5;
+
+let sum  = (n1, n2)=> n1 + n2;
+
+console.log(sum(a, b));
