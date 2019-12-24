9e57f27c4046f6a6ccd321776cba031d8d6a9679
Mengti Sun <mengtisun@gmail.com>
2019-12-24 13:51:02 +1100

    remove old files

--- webpack/notUsed/path.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-// path.resolve('aaa/bbb', '../ccc', './xxx/yyy', '../a.txt');
-// __dirname: current path
-console.log(path.resolve(__dirname, 'dest'));
--- webpack/src/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../css/main.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import $ from 'jquery';
-
-$(function() {
-  $('body').css('background', '#CCC');
-
-  console.log(process.env.NODE_ENV)
-})
