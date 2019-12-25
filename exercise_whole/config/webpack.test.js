41c797074f103893713d4a38d9a779f70663b956
Mengti Sun <mengtisun@gmail.com>
2019-12-25 11:33:40 +1100

    test config

---
+++ exercise_whole/config/webpack.test.js
@@ -0,0 +1,14 @@
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "bundle.js"
+  },
+  plugins: [
+    new StylelintPlugin({
+      files: ['**/*.css', '**/*.less', '**/*.html', '**/*.htm', '**/*.vue', '**/*.scss']
+    }),
+  ],
+  devtool: "source-map"
+}
--- exercise_whole/webpack.config.js
+++ exercise_whole/webpack.config.js
@@ -2,16 +2,63 @@ const path = require('path');
 
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index',
-  output: {
-    path: path.resolve(__dirname, 'dest'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new StyleLintPlugin({
-      files: ['**/*.css', '**/*.less', '**/*.html', '**/*.htm', '**/*.vue', '**/*.scss']
-    }),
-  ]
+module.exports = function(env, argv) {
+  env = env || {development: true};
+
+  let conf = null;
+  if (env.production) {
+    conf = require("./config/webpack.production");
+  } else if (env.development) {
+    conf = require("./config/webpack.development");
+  } else {
+    conf = require("./config/webpack.test");
+  }
+
+  return {
+    entry: './src/js/index',
+    ...conf,
+    module: {
+      rules: [
+        // JS
+        {test: /\.(js|jsx)$/i, use: [{
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }, {
+          loader: "eslint-loader",
+          options: {
+            exclude: /node_modules|bower_modules/
+          }
+        }]}, 
+        // CSS
+        {test: /\.css$/i, use: ["style-loader", "css-loader", {
+          loader: "postcss-loader",
+          options: {
+            plugins: [
+              require("autoprefixer")
+            ]
+          }
+        }]},
+        // LESS
+        {test: /\.less$/i, use: ["style-loader", "css-loader","less-loader"]},
+        // images
+        {test: /\.(png|jpg|gif)$/i, use: {
+          loader: "url-loader",
+          options: {
+            outputPath: 'imgs/',
+            limit: 4*1024
+          }
+        }},
+        // fonts
+        {test: /\.(eot|svg|ttf|woff|woff2)$/i, use: {
+          loader: "url-loader",
+          options: {
+            outputPath: 'imgs/',
+            limit: 4*1024
+          }
+        }}
+      ]
+    }
+  }
 }
